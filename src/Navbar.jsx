1f6e0205908c529ca05fe144b515bb42554d6419
Jonibek <jonibek@example.com>
2025-08-17 12:22:32 +0500

    o'zgartirishlar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
     <nav className="vit">
       <h2 className="logo">Fonte</h2>
 
-      {/* Burger tugma */}
+      
       <div className="burger" onClick={() => setIsOpen(!isOpen)}>
         ☰
       </div>
 
-      {/* Menyu */}
+
       <ul className={isOpen ? "nav-links open" : "nav-links"}>
         <li>
           <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
